257a9ca0b4665eb053f7a873e43caeb8d1159649
DescPyDev <vechniyadam@gmail.com>
2025-09-26 13:50:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ async function handleFormSubmit(e) {
     document.getElementById('errorMessage').style.display = 'none';
 
     try {
-        const url = "https://cors-anywhere.herokuapp.com/https://ai99sr-95-26-207-148.ru.tuna.am/test";
+        const url = "https://api.allorigins.win/raw?url=" + encodeURIComponent("https://ai99sr-95-26-207-148.ru.tuna.am/test");
         const payload = {
             name: "Хуесос",
             age: 16
@@ -232,5 +232,6 @@ document.addEventListener('DOMContentLoaded', function() {
         input.addEventListener('input', calculateTotals);
     });
 });
+
 
 
