31d6eb68f4ee03f93b3e2a3629c284ede6fbe1f3
DescPyDev <vechniyadam@gmail.com>
2025-09-28 20:12:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -223,13 +223,13 @@ function serializeFormData(formData) {
     const payload = {
         type: "liability",
         fullname: {
+            secondname: lastName, 
             name: firstName,
-            secondname: lastName,
             surname: middleName
         },
         marital_status: maritalStatus,
         gender: gender,
-        number_of_dependence: numberOfDependents, // Добавлен новый ключ
+        number_of_dependence: numberOfDependents,
         property: property,
         sources_of_official_income: sources_of_official_income,
         monthly_expenses: monthly_expenses,
@@ -317,5 +317,6 @@ document.addEventListener('DOMContentLoaded', function() {
         input.addEventListener('input', calculateTotals);
     });
 });
+
 
 
